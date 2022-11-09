bc902d0e3d868258e54589ae936d3e9b8b843164
tomaspopik <tpopik1999@gmail.com>
2022-11-09 08:58:14 +0100

    Suppress delete

--- app/src/main/java/sk/stu/fei/mobv/MainActivity.kt
+++ app/src/main/java/sk/stu/fei/mobv/MainActivity.kt
@@ -1,20 +1,15 @@
 package sk.stu.fei.mobv
 
-import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupActionBarWithNavController
 
-private const val TAG = "MainActivity"
-
-
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
 
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
